d432ddc8ccdbc0335757fee78625dbb8155cc90e
Sureshbabu <dsureshbabu@BANL15b18a2a7.local>
2017-05-19 10:42:29 +0530

    Initial Commit.

---
+++ src/test/java/dummy/dummyProjectv1/Test2.java
@@ -0,0 +1,14 @@
+package dummy.dummyProjectv1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		WebDriver d=new FirefoxDriver();
+		d.get("https://www.yahoo.com");
+
+	}
+
+}
